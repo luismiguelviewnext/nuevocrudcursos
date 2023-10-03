23dfd959d81139616ff95741d3bc744c1b3a8e52
luismiguelviewnext <135017297+luismiguelviewnext@users.noreply.github.com>
2023-10-03 09:51:14 +0200

    cambios sql

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO `bd_nuevocursos`.`cursos` (`codCurso`, `nombre`, `duracion`, `precio`) VALUES ('1', 'Matematicas', '50', '100');
-INSERT INTO `bd_nuevocursos`.`cursos` (`codCurso`, `nombre`, `duracion`, `precio`) VALUES ('2', 'Lenguaje', '40', '200');
-INSERT INTO `bd_nuevocursos`.`cursos` (`codCurso`, `nombre`, `duracion`, `precio`) VALUES ('3', 'Inglés', '60', '300');
-INSERT INTO `bd_nuevocursos`.`cursos` (`codCurso`, `nombre`, `duracion`, `precio`) VALUES ('4', 'Literatura', '80', '400');
-INSERT INTO `bd_nuevocursos`.`cursos` (`codCurso`, `nombre`, `duracion`, `precio`) VALUES ('5', 'Música', '70', '500');
+INSERT INTO `bd_nuevocursos`.`cursos` (`cod_curso`, `nombre`, `duracion`, `precio`) VALUES ('1', 'Matematicas', '50', '100');
+INSERT INTO `bd_nuevocursos`.`cursos` (`cod_curso`, `nombre`, `duracion`, `precio`) VALUES ('2', 'Lenguaje', '40', '200');
+INSERT INTO `bd_nuevocursos`.`cursos` (`cod_curso`, `nombre`, `duracion`, `precio`) VALUES ('3', 'Inglés', '60', '300');
+INSERT INTO `bd_nuevocursos`.`cursos` (`cod_curso`, `nombre`, `duracion`, `precio`) VALUES ('4', 'Literatura', '80', '400');
+INSERT INTO `bd_nuevocursos`.`cursos` (`cod_curso`, `nombre`, `duracion`, `precio`) VALUES ('5', 'Música', '70', '500');
 
 
